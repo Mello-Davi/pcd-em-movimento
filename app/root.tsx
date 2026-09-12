11f052871f07206a2ff0f2f13db0f73d8139fa0f
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-09-12 06:25:11 -0300

    feat(ui): add image.png as site favicon

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,11 @@ import type { Route } from "./+types/root";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
+  {
+    rel: "icon",
+    type: "image/png",
+    href: "/image.png",
+  },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
